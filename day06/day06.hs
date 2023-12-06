f6beef4c22ded7540d604461178b0be9b6063423
kyselica12 <dano.kyselica@gmail.com>
2023-12-06 08:55:53 +0100

    Binary search

--- day06/day06.hs
+++ day06/day06.hs
@@ -2,6 +2,7 @@
 
 import Data.List.Split (splitOn)
 import Data.List
+import System.TimeIt
 
 parseInput :: String -> [(Int, Int)]
 parseInput  = map (\[x,y] -> (x,y)) . transpose . map (map read . tail.words) . lines
@@ -12,10 +13,33 @@ parseInput2  = (\[x,y] -> (x,y)) .  map (read.concat) . map (tail.words) . lines
 boatRace :: Int ->  Int -> Int
 boatRace t d = length $ takeWhile (>d) $ dropWhile (<=d) $ map (\x-> x * (t-x)) [0..t]
 
-part1 :: [(Int, Int)] -> Int
-part1 races = product $ map (uncurry boatRace) races
+binarySearch :: Int -> Int -> (Int -> Bool) -> Int
+binarySearch low high goLeft
+    | low == high = low
+    | goLeft mid = binarySearch low mid goLeft
+    | otherwise = binarySearch (mid+1) high goLeft
+    where mid = (low + high) `div` 2
 
-part2 = boatRace
+boatRaceBS :: Int -> Int -> Int
+boatRaceBS t d = end - start
+    where
+        start = binarySearch 0 t (\x -> x*(t-x) > d)
+        end = binarySearch start t (\x -> x*(t-x) <= d)
+
+part1 :: [(Int, Int)] -> Int
+part1 races = product $ map (uncurry boatRaceBS) races
+
+-- 13.8s
+-- drop from start -> takeWhile > d 
+-- part2 = boatRace
+
+-- 22.39 s
+-- drop from start -> reverse -> drop 
+-- part2 t d = length $ reverse $ dropWhile (<=d) $ map (\x-> x * (t-x)) [0..t] 
+
+-- Instanteous :)
+part2 :: Int -> Int -> Int
+part2 = boatRaceBS
 
 main :: IO ()
 main = do
@@ -23,6 +47,6 @@ main = do
     let parsed = parseInput input
     let parsed2 = parseInput2 input
     print "Day 06"
-    print $ "Part 1: " ++ (show $ part1 parsed)
-    print $ "Part 2: " ++ show (uncurry part2 parsed2)
+    timeIt $ print $ "Part 1: " ++ show (part1 parsed)
+    timeIt $ print $ "Part 2: " ++ show (uncurry part2 parsed2)
 
