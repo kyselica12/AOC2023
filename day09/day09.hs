0691a0b5f363a42655144daf32584b4791cd36d8
kyselica12 <dano.kyselica@gmail.com>
2023-12-09 11:13:46 +0100

    Refactor code to use point-free style

--- day09/day09.hs
+++ day09/day09.hs
@@ -6,5 +6,5 @@ next s@(x:xs) = if any (/=0) s then x + next (zipWith (-) s (tail s)) else x
 main :: IO ()
 main = do
     parsed <- map (map read . words) . lines <$> readFile "day09/input.txt"
-    print $ "Part 1: " ++ show (foldl (\a x -> a + next (reverse x)) 1 parsed)
-    print $ "Part 2: " ++ show (foldl (\a x -> a + next x) 1 parsed)
+    print $ "Part 1: " ++ show ((sum. map next . reverse) parsed)
+    print $ "Part 2: " ++ show ((sum . map next) parsed)
