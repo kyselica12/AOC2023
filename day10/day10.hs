8b09cc79f74116e179a539caeb86e791d6b50c7e
kyselica12 <dano.kyselica@gmail.com>
2023-12-10 18:57:06 +0100

    Part2 using Pick's theorem

--- day10/day10.hs
+++ day10/day10.hs
@@ -6,6 +6,9 @@ import Text.Parsec
 import Data.Array (array, Array, (!), bounds, indices, listArray, assocs)
 import qualified Data.Set as S
 import Data.Maybe (catMaybes)
+import GHC.Data.StringBuffer (StringBuffer(len))
+import Debug.Trace (trace)
+import System.TimeIt
 
 type Pos = (Int, Int)
 
@@ -48,29 +51,7 @@ move arr from@(f1, f2) p@(p1, p2) =
             _ -> error "Invalid move"
         ((minI, minJ), (maxI, maxJ)) = bounds arr
         in if minI <= i' && i' <= maxI && minJ <= j' && j' <= maxJ then Just (i', j') else Nothing
-        -- in trace (show (f1, f2)++ " -> " ++ show (p1,p2)  ++ show (arr!p) ++ " ->" ++ show (i', j')) (if minI <= i' && i' <= maxI && minJ <= j' && j' <= maxJ then Just (i', j') else Nothing)
 
-move' :: Array Pos Char -> Pos -> Pos -> Maybe Pos
-move' arr p@(p1,p2) c@(c1,c2) =
-    let (bI,bJ) = snd (bounds arr)
-    in do
-        (i',j') <- res 
-        if 0 <= i' && i' < bI && 0 <= j' && j' < bJ then return (i', j') 
-        else Nothing
-    where
-        d@(dx, dy) = (c1-p1, c2-p2)
-        either (op1, r1) (op2,r2)
-          | d == op1 = Just (c1+fst r1, c2+snd r1)
-          | d == op2 = Just (c1+fst r2, c2+snd r2)
-          | otherwise = Nothing
-        res = case arr ! p of
-            '|' -> either ((1,0),(1,0)) ((-1,0),(-1,0))
-            '-' -> either ((0,1),(0,1)) ((0,-1),(0,-1))
-            'L' -> either ((0,1),(-1,0)) ((1,0),(0,1))
-            'J' -> either ((0,1),(-1,0)) ((1,0),(0,-1))
-            '7' -> either ((0,1),(1,0)) ((-1,0),(0,-1))
-            'F' -> either ((0,-1),(1,0)) ((-1,0),(0,1))
-            _ -> Nothing
 
 part1 :: (Pos, Array Pos Char) -> Int
 part1 (s@(s1, s2), arr) =
@@ -90,6 +71,7 @@ expandPath ((x1,x2):(y1,y2):rest) = (x1*2,x2*2):(x1*2+y1-x1, x2*2+y2-x2):expandP
 expandPath [(x1,x2)] = [(x1*2,x2*2)]
 
 -- part2 :: (Pos, Array Pos Char) -> Int
+part2 :: ((Int, Int), Array (Int, Int) Char) -> Int
 part2 (s@(s1, s2), arr) =
     S.size . S.filter (\(i,j) -> even i && even j) . S.difference allPosSet $ (S.union loopPos outBlobs)
     where
@@ -113,12 +95,29 @@ exploreBlob arr loopPos blobPos p@(p1,p2) =
             where
                 neighbors = [(p1+i, p2+j) | (i,j) <- [(-1,0),(1,0), (0,1), (0,-1)], p1+i >= 0, p2+j >= 0, p1+i <= maxI*2, p2+j <= maxJ*2]
 
+-- part2PicksTheorem :: ((Int, Int), Array (Int, Int) Char) -> Int
+part2PicksTheorem :: ((Int, Int), Array Pos Char) -> Int
+part2PicksTheorem (s@(s1,s2), arr) = picksTheorem path
+    where 
+        path =  head . catMaybes . map (loop arr [s]) $ [(s1+1,s2), (s1, s2-1)]
+
+trapezoidFormula :: [Pos] -> Int
+trapezoidFormula ((x1,y1):(x2,y2):xs) = (y1+y2)*(x2-x1) + trapezoidFormula ((x2,y2):xs)
+trapezoidFormula _ = 0
+
+picksTheorem :: [Pos] -> Int
+picksTheorem path =  area - b `div` 2 + 1
+    where 
+        area = trapezoidFormula path `div` 2 
+        b = length path - 1
+
 main :: IO ()
 main = do
     input <- readFile "day10/input.txt"
     let parsed = parseInput input
     print "Day 10"
     -- print parsed
-    print $ "Part 1: " ++ show (part1 parsed)
-    print $ "Part 2: " ++ show (part2 parsed)
+    timeIt $ print $ "Part 1: " ++ show (part1 parsed)
+    timeIt $ print $ "Part 2: " ++ show (part2 parsed)
+    timeIt $ print $ "Part 2 Pick's theorem: " ++ show (part2PicksTheorem parsed)
 
