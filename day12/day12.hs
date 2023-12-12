7392d9b3f70102323fc848eda12e6c7e2a6e392e
kyselica12 <dano.kyselica@gmail.com>
2023-12-12 14:28:18 +0100

    Refactor -> memoization speedup

--- day12/day12.hs
+++ day12/day12.hs
@@ -4,43 +4,42 @@ import Data.List
 import Data.List.Split (splitOn)
 import Data.Map qualified as M
 import System.TimeIt (timeIt)
-import Debug.Trace (trace)
 
 parseInput :: String -> [(String, [Int])]
 parseInput = map ((\[m, x] -> (m, map read (splitOn "," x))) . words) . lines
 
+part1 :: [(String, [Int])] -> Int
 part1 = sum . map solveRow
-unfold (ms, ns) = (intercalate "?" (replicate 5 ms), concat (replicate 5 ns))
 
 part2 :: [(String, [Int])] -> Int
 part2 = sum . map (solveRow . unfold)
+    where unfold (ms, ns) = (intercalate "?" (replicate 5 ms), concat (replicate 5 ns))
 
 solveRow :: (String, [Int]) -> Int
-solveRow (input,ns) = mem M.! (ns, 0, input)
-  where
-    mem =
-      M.fromList
-        [ ((n, c, i), aux n c i)
-          | n <- tails ns,
-            c <- [0 .. maximum ns],
-            i <- tails input
-        ]
-    aux [] 0 i = if notElem '#' i then 1 else 0
-    aux [p] c [] = if c == p then 1 else 0
-    aux (p : ps) c [] = 0
-    aux (p : ps) 0 ('.' : is) = mem M.! (p : ps, 0, is)
-    aux (p : ps) c ('.' : is) = if p == c then mem M.! (ps, 0, is) else 0
-    aux (p : ps) c ('#' : is) = if p == c then 0 else mem M.! (p : ps, c + 1, is)
-    aux (p : ps) c ('?' : is) = aux (p : ps) c ('.' : is) + aux (p : ps) c ('#' : is)
-    aux n c i = error $ "impossible " ++ show (n, c, i)
+solveRow (ms, ns) = mem M.! (0, 0, 0)
+    where
+        ln = length ns
+        lm = length ms
+        mem = M.fromList [
+            ((m,n,a), aux m n a c p) |
+            (m,c) <- zip [0..] (ms ++ "X"),
+            (n,p) <- zip [0..] (ns ++ [0]),
+            a <- [0..maximum ns] ]
+        aux m n a c p
+            | n == ln && m == lm && a == 0 = 1
+            | n == ln-1 && a == p && m == lm = 1
+            | n == ln && a == 0 && c /= '#' = mem M.! (m+1, n, 0)
+            | c == '.' && a == 0 = mem M.! (m+1, n, 0)
+            | c == '.' && a == p = mem M.! (m+1, n+1, 0)
+            | c == '#' && a < p = mem M.! (m+1, n, a+1)
+            | c == '?' && a == 0 = mem M.! (m+1, n, 0) + mem M.! (m+1, n, 1)
+            | c == '?' && a == p = mem M.! (m+1, n+1, 0)
+            | c == '?' && a < p = mem M.! (m+1, n, a+1)
+            | otherwise = 0
 
 main :: IO ()
 main = do
-  input <- readFile "day12/input.txt"
-  let parsed = parseInput input
-  let xxx = map (solveRow . unfold) parsed
+  parsed <- parseInput <$> readFile "day12/input.txt"
   print "Day 12"
   timeIt $ print $ "Part 1: " ++ show (part1 parsed)
-  print xxx
-  print $ sum xxx
---   timeIt $ print $ "Part 2: " ++ (show $ part2 parsed)
+  timeIt $ print $ "Part 2: " ++ show (part2 parsed)
