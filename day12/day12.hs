e4de69752d39b4883da788e1eccabc398b313f21
kyselica12 <dano.kyselica@gmail.com>
2023-12-12 16:07:29 +0100

    Speedup using Arrays insead of Map

--- day12/day12.hs
+++ day12/day12.hs
@@ -2,7 +2,7 @@ module Main (main) where
 
 import Data.List
 import Data.List.Split (splitOn)
-import Data.Map qualified as M
+import qualified Data.Array as A
 import System.TimeIt (timeIt)
 
 parseInput :: String -> [(String, [Int])]
@@ -16,25 +16,24 @@ part2 = sum . map (solveRow . unfold)
     where unfold (ms, ns) = (intercalate "?" (replicate 5 ms), concat (replicate 5 ns))
 
 solveRow :: (String, [Int]) -> Int
-solveRow (ms, ns) = mem M.! (0, 0, 0)
+solveRow (ms, ns) = mem A.! (0, 0, 0)
     where
         ln = length ns
         lm = length ms
-        mem = M.fromList [
-            ((m,n,a), aux m n a c p) |
+        mem = A.array ((0,0,0), (lm,ln,maximum ns)) [((m,n,a), aux m n a c p) |
             (m,c) <- zip [0..] (ms ++ "X"),
             (n,p) <- zip [0..] (ns ++ [0]),
             a <- [0..maximum ns] ]
         aux m n a c p
             | n == ln && m == lm && a == 0 = 1
             | n == ln-1 && a == p && m == lm = 1
-            | n == ln && a == 0 && c /= '#' = mem M.! (m+1, n, 0)
-            | c == '.' && a == 0 = mem M.! (m+1, n, 0)
-            | c == '.' && a == p = mem M.! (m+1, n+1, 0)
-            | c == '#' && a < p = mem M.! (m+1, n, a+1)
-            | c == '?' && a == 0 = mem M.! (m+1, n, 0) + mem M.! (m+1, n, 1)
-            | c == '?' && a == p = mem M.! (m+1, n+1, 0)
-            | c == '?' && a < p = mem M.! (m+1, n, a+1)
+            | n == ln && a == 0 && c /= '#' = mem A.! (m+1, n, 0)
+            | c == '.' && a == 0 = mem A.! (m+1, n, 0)
+            | c == '.' && a == p = mem A.! (m+1, n+1, 0)
+            | c == '#' && a < p = mem A.! (m+1, n, a+1)
+            | c == '?' && a == 0 = mem A.! (m+1, n, 0) + mem A.! (m+1, n, 1)
+            | c == '?' && a == p = mem A.! (m+1, n+1, 0)
+            | c == '?' && a < p = mem A.! (m+1, n, a+1)
             | otherwise = 0
 
 main :: IO ()
