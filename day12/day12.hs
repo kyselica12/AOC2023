d3bd9fda56d41c5e105bec00e6c244676605431c
kyselica12 <dano.kyselica@gmail.com>
2023-12-12 10:59:47 +0100

    part 2

--- day12/day12.hs
+++ day12/day12.hs
@@ -1,47 +1,46 @@
-module Main where
+module Main (main) where
 
-import Data.List.Split (splitOn)
 import Data.List
-import Debug.Trace (trace)
+import Data.List.Split (splitOn)
+import Data.Map qualified as M
+import System.TimeIt (timeIt)
 
-parseInput :: String -> [(String,[Int])]
-parseInput = map ((\[m,x] -> (m,map read (splitOn "," x))) . words) . lines
+parseInput :: String -> [(String, [Int])]
+parseInput = map ((\[m, x] -> (m, map read (splitOn "," x))) . words) . lines
 
-solveRow' = solveRow []
+part1 = sum . map solveRow
 
-solveRow :: String -> [Int] -> Int
-solveRow (m:ms) (x:xs)
-    | m == '.' = solveRow  ms (x:xs)
-    | m == '#' = let (n, rest) = splitAt (x-1) ms
-                     ms' = if null rest then [] else tail rest
-                 in if notElem '.' n && length n == x-1 && ( null rest || head rest /= '#')
-                 then  solveRow  ms' xs else 0
-    | m == '?' = solveRow ('#':ms) (x:xs) + solveRow ('.':ms) (x:xs)
-solveRow [] [] =  1
-solveRow [] x =  0
-solveRow x []=  if '#' `notElem` x then 1 else 0
+part2 :: [(String, [Int])] -> Int
+part2 = sum . map (solveRow . unfold)
+  where
+    unfold (ms, ns) = (intercalate "?" (replicate 5 ms), concat (replicate 5 ns))
 
-part1 = sum .  map (uncurry solveRow )
-
-part2 input = c
-    where
-        (ms,ns) = head input
-        a = takeWhile ((>0).fst) $ map (\x -> (solveRow x ns, x)) (tails ms)
-        c = takeWhile ((>0).fst) $ reverse $ map (\x -> (solveRow x ns, x)) (inits ms)
-        minlen = length $ snd $ last a
-        b =takeWhile ((>0).fst) $ map (\x -> (solveRow x ns, x)) (map (++ms) (inits (ms++ms++ms++ms)))
-
-
-
-
+solveRow :: (String, [Int]) -> Int
+solveRow (input,ns) = mem M.! (ns, 0, input)
+  where
+    mem =
+      M.fromList
+        [ ((n, c, i), aux n c i)
+          | n <- tails ns,
+            c <- [0 .. maximum ns],
+            i <- tails input
+        ]
+    aux [] 0 i = if notElem '#' i then 1 else 0
+    aux [p] c [] = if c == p then 1 else 0
+    aux (p : ps) c [] = 0
+    aux (p : ps) 0 ('.' : is) = mem M.! (p : ps, 0, is)
+    aux (p : ps) c ('.' : is) = if p == c then mem M.! (ps, 0, is) else 0
+    aux (p : ps) c ('#' : is) = if p == c then 0 else mem M.! (p : ps, c + 1, is)
+    aux (p : ps) c ('?' : is) = aux (p : ps) c ('.' : is) + aux (p : ps) c ('#' : is)
+    aux n c i = error $ "impossible " ++ show (n, c, i)
 
 main :: IO ()
 main = do
-    input <- readFile "day12/input.txt"
-    let parsed = parseInput input
-    print "Day 12"
-    -- print $ parsed
-    -- print $ uncurry solveRow $ last parsed
-    -- print $ "Part 1: " ++ show (part1 parsed)
-    print $ "Part 2: " ++ (show $ part2 parsed)
-
+  input <- readFile "day12/input.txt"
+  let parsed = parseInput input
+  print "Day 12"
+  -- print $ parsed
+  -- print $ uncurry solveRow $ last parsed
+  timeIt $ print $ "Part 1: " ++ show (part1 parsed)
+  -- print $ uncurry part2 $ head parsed
+  timeIt $ print $ "Part 2: " ++ show (part2 parsed)
