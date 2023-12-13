76243f0d568defc9f892b68d2eaba36c08f59d15
kyselica12 <dano.kyselica@gmail.com>
2023-12-13 13:26:23 +0100

    Refactor for short code

--- day13/day13.hs
+++ day13/day13.hs
@@ -1,43 +1,21 @@
 module Main where
-
 import Data.List.Split (splitOn)
 import Data.List
-import Debug.Trace (trace)
 import Data.Maybe (fromJust)
-import Control.Applicative 
+import Control.Applicative
 
-parseInput :: String -> [[String]]
-parseInput input = map lines $ splitOn "\n\n" input
+-- count number of different elements in two lists 
+difference :: ([String] , [String]) -> Int
+difference (a,b) = (length .filter id . concat )(zipWith (zipWith (/=)) (reverse a) b)
 
-equal (a:as,b:bs) = (a == b) && equal (as,bs)
-equal _ = True
-
-difference1 (a:as,b:bs) = case foldl (\acc (x,y) -> if x/=y then acc+1 else acc) 0 (zip a b) of
-                            0 -> difference1 (as,bs)
-                            1 -> equal (as,bs)
-                            _ -> False
-difference1 _ = False
-
-part1 :: [[String]] -> Int
-part1 = sum . map (solveBlock aux)
-    where
-        aux i block = let (a,b) = splitAt i block in  equal (reverse a, b)
-
-solveBlock :: (Int -> [String] -> Bool) -> [String] -> Int
-solveBlock f block = fromJust $ col <|> ((*100) <$> row) 
-    where 
-        row = find (`f` block) [1..length block-1]
-        col = find (`f` transpose block) [1..length (head block)-1]
-
-part2 :: [[String]] -> Int
-part2 = sum . map (solveBlock aux)
-    where
-        aux i block = let (a,b) = splitAt i block in difference1 (reverse a, b) && not (equal (reverse a, b))
+-- solve for cols and rows
+solveBlock :: (([String],[String]) -> Bool) -> [String] -> Int
+solveBlock f block = fromJust $ 
+    find (\i-> f (splitAt i (transpose block))) [1..length (head block)-1]
+    <|> ((*100) <$> find (\i-> f (splitAt i block)) [1..length block-1])
 
 main :: IO ()
 main = do
-    input <- parseInput <$> readFile "day13/input.txt"
-    print "Day 13"
-    print $ "Part 1: " ++ show (part1 input)
-    print $ "Part 2: " ++ show (part2 input)
-
+    input <- map lines . splitOn "\n\n" <$> readFile "day13/input.txt"
+    print $ "Part 1: " ++ show (sum $ map (solveBlock ((==0).difference)) input)
+    print $ "Part 2: " ++ show (sum $ map (solveBlock ((==1).difference)) input)
