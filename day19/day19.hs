8398fafd61b0333e9458c910e4209cf5facfc102
kyselica12 <dano.kyselica@gmail.com>
2023-12-19 10:39:01 +0100

    day19 solution

--- day19/day19.hs
+++ day19/day19.hs
@@ -1,74 +1,109 @@
-module Main where 
+module Main (main) where
 
 import Data.List.Split (splitOn)
 import Data.List
-import Text.Parsec 
+import Text.Parsec
 import Control.Applicative
+import qualified Data.Map as M
+import Debug.Trace (trace)
 
-dataParser = do
-    -- a <- sepBy1 workflow (char '\n')  
-    a <- many1 (workflow <* (string "\n"))
-    string "\n"
-    -- b <- many1 (many1 (noneOf "\n") <* (char '\n'))
-    b <- sepBy (part) (string "\n")
-    eof
-    return (a,b) 
+data Rule = Rule { p :: String, sign :: Char, val :: Int, dst :: String} deriving (Show)
+data Part = Part { x :: Int, m :: Int, a :: Int, s :: Int } deriving (Show)
+
+dataParser :: Parsec String () ([(String, [Rule], String)], [Part])
+dataParser = (,) <$> (endBy workflow eol <* eol) <*> (sepBy part eol <* eof)
+
+eol = char '\n'
 
 workflow :: Parsec String () (String, [Rule], String)
-workflow = do 
-    name <- many1 letter
-    char '{'
-    rules <- manyTill (rule) (try (lookAhead (many1 letter <* string "}")))
-    x <- many1 letter
-    char '}'
-    return (name, rules, x)
+workflow = (,,) <$> (many1 letter <* char '{') <*>
+           manyTill (rule <* char ',') (try (lookAhead (many1 letter <* string "}"))) <*>
+           many1 letter <* char '}'
 
--- type Rule = Char
--- type Rule = (String, Char, String, String)
-data Rule = Rule { f :: Part -> Bool, dst :: String } 
-rule = do
-    i <- many1 letter 
-    sign <- oneOf "<>="
-    val <- many1 digit
-    char ':'
-    dst <- many1 (noneOf ",}")
-    return (Rule (\p -> (f1 sign) ((f2 i) p) (read val)) dst)
-    where 
-        f1 sign = case sign of
+rule = Rule <$> (many1 letter) <*>
+                (oneOf "<>") <*>
+                (read <$> many1 digit) <*>
+                (char ':' *> many1 letter)
+
+part :: Parsec String () Part
+part = (\[x,m,a,s] -> Part x m a s) . map read <$>
+       (char '{' *> sepBy1 (count 2 anyToken *> many1 digit) (char ',') <* char '}')
+
+parseInput input = either (error.show) id (parse dataParser "ERROR" input)
+
+applyWorkflow :: Part -> ([Rule], String) -> String
+applyWorkflow _ ([], def) = def
+applyWorkflow it@(Part x m a s) (r:rs, def) = if f g (val r) then dst r else applyWorkflow it (rs, def)
+    where
+        f = case sign r of
             '<' -> (<)
             '>' -> (>)
-        f2 i = case i of
+        g = case p r of
             "x" -> x
             "m" -> m
             "a" -> a
             "s" -> s
 
-data Part = Part { x :: Int, m :: Int, a :: Int, s :: Int } deriving (Show)    
+part1 :: ([(String, [Rule], String)], [Part]) -> Int
+part1 (workflows, items) = sum $ map  (aux "in") items
+    where
+        mem = M.fromList $ map (\(name, rules, dst) -> (name, (rules, dst))) workflows
+        aux w it = case w of
+            "A" -> x it + m it + a it + s it
+            "R" -> 0
+            _ -> trace (show w) aux (applyWorkflow it (mem M.! w)) it
 
-part :: Parsec String () Part
-part = do
-    char '{'
-    x <- read <$> (string "x=" *> many1 digit)
-    char ','
-    m <- read <$> (string "m=" *> many1 digit)
-    char ','
-    a <- read <$> (string "a=" *> many1 digit)
-    char ','
-    s <- read <$> (string "s=" *> many1 digit)
-    char '}'
-    return (Part x m a s)
+data Range = R { l :: Int, r :: Int } | Nil deriving (Show, Eq)
+data ItemRange = ItemRange { xr :: Range, mr :: Range, ar :: Range, sr :: Range } deriving (Show)
 
-parseInput input = either (error.show) id (parse dataParser "ERROR" input)
+applyRuleItemRange :: Rule -> ItemRange -> (ItemRange, ItemRange)
+applyRuleItemRange rule@(Rule p s v d) ir =
+    case p of
+        "x" -> let (r1,r2) = apllyRuleRange rule p (xr ir) in (ir { xr = r1 }, ir { xr = r2 })
+        "m" -> let (r1,r2) = apllyRuleRange rule p (mr ir) in (ir { mr = r1 }, ir { mr = r2 })
+        "a" -> let (r1,r2) = apllyRuleRange rule p (ar ir) in (ir { ar = r1 }, ir { ar = r2 })
+        "s" -> let (r1,r2) = apllyRuleRange rule p (sr ir) in (ir { sr = r1 }, ir { sr = r2 })
 
-part1 = undefined
+apllyRuleRange :: Rule -> String -> Range -> (Range, Range)
+apllyRuleRange (Rule p s v d) x r@(R a b) =
+    if p /= x then (Nil, r) else
+    case  (a `f` v, b `f` v) of
+        (True, True) -> (r, Nil)
+        (False, False) -> (Nil, r)
+        (True, False) -> (R a (v-1), R v b)
+        (False, True) -> (R (v+1) b, R a v)
+    where
+        f = case s of
+            '<' -> (<)
+            '>' -> (>)
 
-part2 = undefined
+isNil :: ItemRange -> Bool
+isNil (ItemRange x m a s) = x == Nil && m == Nil && a == Nil && s == Nil
+
+rangeSize (R a b) = b - a +1
+itemRangeCount (ItemRange x m a s) = product $ map rangeSize [x,m,a,s]
+
+part2 (workflows, _) = sum $ map itemRangeCount $ applyRange "in" (ItemRange (R 1 4000) (R 1 4000) (R 1 4000) (R 1 4000))
+    where
+        mem = M.fromList $ map (\(name, rules, dst) -> (name, (rules, dst))) workflows
+        aux ([],d) range = [(d, range)]
+        aux ((rule:rs),d) range =
+            let (r1,r2) = applyRuleItemRange rule range in
+            if isNil range then [] else  (dst rule, r1) : aux (rs,d) r2
+        -- applyRange :: String -> String -> Range -> Int
+        applyRange "A" r = [r]
+        applyRange "R" r = []
+        applyRange w range =
+            if isNil range then []
+            else concatMap (uncurry applyRange) (aux (mem M.! w) range)
+
 
 
 main :: IO ()
 main = do
-    input <- parseInput <$> readFile "day19/example.txt"
+    input <- parseInput <$> readFile "day19/input.txt"
     print "Day 19"
+    -- print $ input
     -- print $ "Part 1: " ++ (show $ part1 input)
-    -- print $ "Part 2: " ++ (show $ part2 input)
+    print $ "Part 2: " ++ (show $ part2 input)
 
