af3a67e066a9fc2441dbce6ef1ebc9fa4dd0cb40
kyselica12 <dano.kyselica@gmail.com>
2023-12-20 12:13:58 +0100

    day20 solution

--- day20/day20.hs
+++ day20/day20.hs
@@ -1,14 +1,16 @@
-module Main where
+module Main (main) where
 
 import Data.List.Split (splitOn)
 import Data.List
 import Text.Parsec hiding (State)
 import Debug.Trace (trace)
-import Language.Haskell.TH (isInstance)
+import qualified Data.Map as M
+import Data.Maybe (fromJust)
 
-data State = B | F {on::Bool} | C {mem::[Bool]} deriving (Show, Eq)
+data State = B | F {on::Bool} | C {mem::M.Map String Bool} deriving (Show, Eq)
 
 data Module = M {name::String, to::[String], s::State} deriving (Show, Eq)
+data Signal = L | H deriving (Show, Eq)
 
 -- inputP :: Parsec String () [String]
 inputP = do
@@ -17,8 +19,7 @@ inputP = do
     return lines
 
 moduleP = do
-    t <- lookAhead (anyToken)
-    trace (show t) $ return ()
+    t <- lookAhead anyToken
     name <- many1 (noneOf " ")
     string " -> "
     x <- many1 (noneOf "\n")
@@ -29,29 +30,63 @@ moduleP = do
             let to = splitOn ", " x
             in case t of
                 '%' -> M (getName t name) to (F False)
-                '&' -> M (getName t name) to (C [])
+                '&' -> M (getName t name) to (C M.empty)
                 _ -> M (getName t name) to B
-
-
 
 parseInput input =
     let parsed = either (error.show) id (parse inputP "ERROR" input)
-        cons = map name $ filter (\(M _ _ s) -> s /= B && s /= F False) parsed
-        other = filter ((`notElem` cons).name) parsed
-    in trace (show other) parsed
+        (cons, others) = partition isCons parsed
+        cons' = map (\m@(M n _ s) -> m{s=s{mem= M.fromList (map (\m -> (name m, False)) $ filter (\m -> n `elem` to m ) parsed)}}) cons
+        modules = M.fromList $ map (\m -> (name m, m)) $ cons' ++ others
+    in  modules
+        where
+            isCons (M _ _ s) = case s of
+                B -> False
+                F _ -> False
+                C _ -> True
 
+recieveSignal m@(M n to B) from signal = (Just signal, m)
+recieveSignal m@(M n to (F on)) from H = (Nothing, m)
+recieveSignal m@(M n to (F on)) from L = if on then (Just L, m{s=F False}) else (Just H, m{s=F True})
+recieveSignal m@(M n to (C mem)) from signal =
+    let mem' = M.insert from (signal == H) mem
+    in if and (M.elems mem') then (Just L, m{s=C mem'}) else (Just H, m{s=C mem'})
 
-part1 = undefined
+oneRound modules = aux modules [(L, "INPUT","broadcaster")] 0 0 []
+    where
+        aux ms ((signal, src, dst):xs) cl ch switched =
+            let (signal', m') = recieveSignal (ms M.! dst) src signal
+                ms' = M.insert dst m' ms
+                next = map (fromJust signal', dst,)  (to m')
+                cl' = if signal == L then cl+1 else cl
+                ch' = if signal == H then ch+1 else ch
+                i' = if dst == "xn" && signal == H then src : switched else switched
+            in
+            if not (M.member dst ms) then  aux ms xs cl' ch' i'
+            else case signal' of
+                Nothing -> aux ms' xs cl' ch' i'
+                Just signal' -> aux ms' (xs++next) cl' ch' i'
+        aux ms [] cl ch i  = (ms,cl, ch,i)
 
-part2 = undefined
+part1 parsed =
+    let (_,a,b) = foldl (\(ms,a,b) _ -> let (ms',a',b',_) = oneRound ms in (ms',a+a', b+b')) (parsed,0,0) [1..1000]
+    in a*b
+
+part2 parsed = M.foldl lcm 1 $ aux parsed 0 counts
+    where
+        aux ms i counts =
+            let (ms',_,_,x) = oneRound ms
+                mem' = mem (s (ms' M.! "xn"))
+                counts' = foldl (\m k -> M.insert k (i+1) m) counts x
+            in if all (>0) (M.elems counts') then counts' else aux ms' (i+1) counts'
+        counts = M.fromList $ map (, 0) $ M.keys ( mem (s (parsed M.! "xn")))
 
 main :: IO ()
 main = do
     -- input <- readFile "day20/input.txt"
-    input <- readFile "day20/example.txt"
+    input <- readFile "day20/input.txt"
     let parsed = parseInput input
     print "Day 20"
-    print parsed
-    -- print $ "Part 1: " ++ (show $ part1 parsed)
-    -- print $ "Part 2: " ++ (show $ part2 parsed)
-
+    -- print parsed
+    print $ "Part 1: " ++ (show $ part1 parsed)
+    print $ "Part 2: " ++ (show $ part2 parsed)
