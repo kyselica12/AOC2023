07a8be3ad9700511ae8c70576e6fd6bf04c7a70a
kyselica12 <dano.kyselica@gmail.com>
2023-12-22 13:47:00 +0100

    day22 solution

--- day22/day22.hs
+++ day22/day22.hs
@@ -25,17 +25,23 @@ fallOne (Block x y z) = Block x y (fall z)
 falling :: [Block] -> Block -> (Block, [Block])
 falling onGround block =
     let block' = fallOne block
-        (onSameLevel,rest) = span (\b -> (end.z) b == (start.z) block') onGround
+        (onSameLevel,rest) = span (\b -> (end.z) b >= (start.z) block') onGround
         supportedBy = filter (inCollision block') onSameLevel
-    in if any (inCollision block') onSameLevel || (start.z) block' == 0
+    in if not (null supportedBy) || (start.z) block' == 0
     then (block, supportedBy) else falling rest block'
 
--- settle :: [Block] -> [(Block, [Block])] 
+settle :: [Block] -> [Block] -> [(Block, [Block])]
 settle (b:toProcess) onGround =
     let (b', supportedBy) = falling onGround b
     in (b', supportedBy) : settle toProcess (sortOn ((*(-1)).end.z) (b':onGround))
 settle [] _ = []
 
+settle2 :: [Block] -> [Block] -> Int
+settle2 (b:toProcess) onGround =
+    let (b', supportedBy) = falling onGround b
+        score = if b' /= b then 1 else 0
+    in score + settle2 toProcess (sortOn ((*(-1)).end.z) (b':onGround))
+settle2 [] _ = 0
 -- part1 blocks = map fst $  filter canDisintegrate settled
 part1 blocks = length $ filter canDisintegrate settled
     where
@@ -48,14 +54,31 @@ part1 blocks = length $ filter canDisintegrate settled
             in all (\b' -> length (supportedBy M.! b') > 1 ) su
         aux mem (b, sp) = foldl (\mem' b' -> M.insert b' (b : M.findWithDefault [] b' mem') mem') mem sp
 
-part2 = undefined
+part2 blocks = resss
+    where
+        resss = sum $ map disintegrateScore [0..length settled - 1]
+        ressss = helper 1 [] settledBlocks
+        sortedBlocks = sortOn (start.z) blocks
+        settled = settle sortedBlocks []
+        supportedBy = M.fromList settled
+        supports = foldl (\mem (b,sp) -> aux mem (b,sp)) M.empty settled
+        settledBlocks = sortOn (start.z) $ map fst settled
+        disintegrateScore i =
+            let (first,x:second) = splitAt i settledBlocks 
+                blocks = first ++ second
+                res = settle2 second (sortOn ((*(-1)).end.z) first)
+            in trace (show (i,x) ++ " " ++ show res) res
+        helper i prev (x:xs) = trace (show i) settle2 (xs) prev + helper (i+1) (prev ++ [x]) xs
+        helper i _ [] = 0
+
+        aux mem (b, sp) = foldl (\mem' b' -> M.insert b' (b : M.findWithDefault [] b' mem') mem') mem sp
 
 main :: IO ()
 main = do
-    input <- readFile "day22/example.txt"
+    input <- readFile "day22/input.txt"
     let parsed = parseInput input
     print "Day 22"
     -- print parsed
     print $ "Part 1: " ++ (show $ part1 parsed)
-    -- print $ "Part 2: " ++ (show $ part2 parsed)
+    print $ "Part 2: " ++ (show $ part2 parsed)
 
